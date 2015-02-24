484ab49a6fe6aec06f161ffafa4a7451f29611fd
maverickg <guojq28@gmail.com>
2015-02-24 11:15:28 -0500

    build script

--- b.sh
+++ b.sh
@@ -1,6 +1,34 @@
 #!/bin/bash 
-git clone https://github.com/rstan.git 
 cd rstan
-git submodule update --init --remote
+git submodule update --init --remote --recursive
+sudo apt-get install r-base-core \
+  devscripts gfortran-multilib gfortran-doc gfortran-4.8-multilib \
+  gfortran-4.8-doc libgfortran3-dbg libdigest-hmac-perl libgssapi-perl \
+  libdata-dump-perl libcrypt-ssleay-perl ncurses-doc libauthen-ntlm-perl \
+  python-rsvg python-cairo ess r-doc-info r-doc-pdf r-mathlib r-base-html \
+  texlive-base texlive-latex-base texlive-generic-recommended \
+  texlive-fonts-recommended texlive-fonts-extra texlive-extra-utils \
+  texlive-latex-recommended texlive-latex-extra texinfo texi2html \
+  build-essentiala \
+  tcl-tclreadline \
+R CMD build StanHeaders/
+
+stanheadtargz=`find StanHeaders*.tar.gz`
+
+lookforverfile=`tar tf ${stanheadtargz} | grep stan/version.hpp`
+
+if [ -n "$lookforverfile" ]; then
+    echo "stan/version.hpp is not found in StanHeaders pkg"
+    exit 2
+fi
+
+R CMD INSTALL ${stanheadtargz}
+
+R -q -e "options(repos=structure(c(CRAN = 'http://cran.rstudio.com'))); for (pkg in c('inline', 'Rcpp', 'RcppEigen', 'RUnit', 'BH', 'RInside')) if (!require(pkg, character.only = TRUE))  install.packages(pkg, dep = TRUE); sessionInfo()"
+
 cd rstan
 make check
+
+cd tests
+R -q -f runRunitTests.R --args ../rstan.Rcheck
+
