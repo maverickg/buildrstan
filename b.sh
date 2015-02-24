71597fd0ef8c48f64d2944f957a59b0efecc1aee
maverickg <guojq28@gmail.com>
2015-02-24 18:43:14 +0000

    set R_LIBS at the beginning

--- b.sh
+++ b.sh
@@ -4,14 +4,17 @@ dd if=/dev/zero of=.swapfile bs=2048 count=1M
 mkswap .swapfile
 sudo swapon .swapfile
 
+mkdir -p ~/rlib
+export  R_LIBS="~/rlib"
+
 cd rstan
 git submodule update --init --remote --recursive
 sudo apt-get -qq update
 sudo apt-get -qq -y install r-base-core \
-  texlive-base texlive-latex-base texlive-generic-recommended \
-  texlive-fonts-recommended texlive-fonts-extra texlive-extra-utils \
-  texlive-latex-recommended texlive-latex-extra texinfo \
-  build-essential \
+# texlive-base texlive-latex-base texlive-generic-recommended \
+# texlive-fonts-recommended texlive-fonts-extra texlive-extra-utils \
+# texlive-latex-recommended texlive-latex-extra texinfo \
+# build-essential \
   ccache
 
 R CMD build StanHeaders/
@@ -27,8 +30,6 @@ fi
 
 R CMD INSTALL ${stanheadtargz}
 
-mkdir -p ~/rlib
-export  R_LIBS="~/rlib"
 R -q -e "options(repos=structure(c(CRAN = 'http://cran.rstudio.com'))); for (pkg in c('inline', 'Rcpp', 'RcppEigen', 'RUnit', 'BH', 'RInside')) if (!require(pkg, character.only = TRUE))  install.packages(pkg, dep = TRUE); sessionInfo()"
 
 mkdir -p ~/.R/
