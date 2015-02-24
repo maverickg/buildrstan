fa906a6cc0560e369eaa26495b80d2f2a4aee6b8
maverickg <guojq28@gmail.com>
2015-02-24 14:24:49 -0500

    add wait4.sh

--- b.sh
+++ b.sh
@@ -1,21 +1,30 @@
 #!/bin/bash 
 
 dd if=/dev/zero of=.swapfile bs=2048 count=1M
-mkswap .swapfile
-sudo swapon .swapfile
+mkswap ~/.swapfile
+sudo swapon ~/.swapfile
 
 mkdir -p ~/rlib
 export  R_LIBS="~/rlib"
 
 cd rstan
-git submodule update --init --remote --recursive
+git submodule update -q --init --remote --recursive
 sudo apt-get -qq update
 sudo apt-get -qq -y install r-base-core \
+  texlive-latex-base texlive-base  xzdec \
 # texlive-base texlive-latex-base texlive-generic-recommended \
 # texlive-fonts-recommended texlive-fonts-extra texlive-extra-utils \
 # texlive-latex-recommended texlive-latex-extra texinfo \
 # build-essential \
   ccache
+
+sudo tlmgr init-usertree
+sudo tlmgr update --self 
+sudo tlmgr install inconsolata upquote courier courier-scaled helvetic \
+                   verbatimbox readarray ifnextok multirow fancyvrb url \
+                   titlesec booktabs tex4ht
+# font
+sudo tlmgr install ec times
 
 R CMD build StanHeaders/
 
@@ -36,9 +45,16 @@ mkdir -p ~/.R/
 echo "CXX = ccache `R CMD config CXX`" > ~/.R/Makevars
 more ~/.R/Makevars
 
+echo "CXX = ccache `R CMD config CXX`" >> ./rstan/rstan/R_Makevars
+more ./rstan/rstan/R_Makevars
+
 cd rstan
-make check
+make check & ../../wait4.sh $!
 
 cd tests
 R -q -f runRunitTests.R --args ../rstan.Rcheck
+
+
+## post building, not important
+sudo swapoff ~/.swapfile
 
---
+++ wait4.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+PROCESS="$1"
+while :
+do
+  RESULT=`ps -p ${PROCESS} -o comm=`
+ 
+  if [ "${RESULT:-null}" = null ]; then
+    break
+  else
+    echo "-"
+    sleep 20
+  fi
+done
