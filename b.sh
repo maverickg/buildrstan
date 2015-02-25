b0cca8f46f300ecf82b670e1787e9bee5b317ef2
maverickg <guojq28@gmail.com>
2015-02-25 12:50:59 -0500

    set the name of stan branch to the name of current buildrstan branch

--- b.sh
+++ b.sh
@@ -1,26 +1,22 @@
 #!/bin/bash 
 
-echo "Usage: $0 [stan repo branch name, default to master]"
-
 mkdir -p ~/rlib
 export  R_LIBS="~/rlib"
 
-STAN_REMO_BRANCH=master
-if [ $# -gt 0 ]; then
-STAN_REMO_BRANCH=develop
-fi
+STAN_REPO_BRANCH=`git rev-parse --abbrev-ref HEAD`
+RSTAN_REPO_BRANCH=develop
 
 dd if=/dev/zero of=~/.swapfile bs=2048 count=1M
 mkswap ~/.swapfile
 sudo swapon ~/.swapfile
 
 
-git config -f .gitmodules submodule.rstan.branch develop
+git config -f .gitmodules submodule.rstan.branch ${RSTAN_REPO_BRANCH}
 git submodule update --init --remote
 git submodule status
 
 cd rstan
-git config -f .gitmodules submodule.stan.branch ${STAN_REMO_BRANCH}
+git config -f .gitmodules submodule.stan.branch ${STAN_REPO_BRANCH}
 git submodule update --init --remote --recursive
 git submodule status
 sudo apt-get -qq update
