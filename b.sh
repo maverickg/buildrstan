1b3dc22bf0fc88cdccc0f7cb3f3f5ee4b44f68e5
maverickg <guojq28@gmail.com>
2015-02-25 14:25:11 -0500

    use stan develop branch

--- b.sh
+++ b.sh
@@ -4,6 +4,7 @@ mkdir -p ~/rlib
 export  R_LIBS="~/rlib"
 
 STAN_REPO_BRANCH=`git rev-parse --abbrev-ref HEAD`
+STAN_REPO_BRANCH=develop
 RSTAN_REPO_BRANCH=debug/test_grad_crash
 
 grepstanbranch=`git ls-remote --heads https://github.com/stan-dev/stan.git | grep "/${STAN_REPO_BRANCH}"`
