021b720f82d9847ba2fb92f6162fd2a74edf5e2e
maverickg <guojq28@gmail.com>
2015-03-04 15:47:42 -0500

    install more tex package through apt

--- b.sh
+++ b.sh
@@ -28,24 +28,25 @@ git submodule status
 
 cd rstan
 git config -f .gitmodules submodule.stan.branch ${STAN_REPO_BRANCH}
-git submodule update --remote --recursive
+git submodule update --remote
 git submodule status
-sudo apt-get -qq update
-sudo apt-get -qq -y install r-base-core qpdf texlive-latex-base texlive-base  xzdec texinfo ccache
 
 mkdir -p "${SEMAPHORE_CACHE_DIR}/.ccahe"
 export CCACHE_DIR="${SEMAPHORE_CACHE_DIR}/.ccahe"
+
+sudo apt-get -qq update
+sudo apt-get -qq -y install r-base-core qpdf texlive-latex-base texlive-base  xzdec texinfo ccache tex4ht texlive-fonts-extra
+sudo tlmgr init-usertree
+sudo tlmgr update --self 
+sudo tlmgr install upquote courier courier-scaled helvetic \
+                   verbatimbox readarray ifnextok multirow fancyvrb url \
+                   titlesec booktabs
 
 mkdir -p ~/.R/
 echo "CXX = ccache `R CMD config CXX`" > ~/.R/Makevars
 more ~/.R/Makevars
 echo "_R_CHECK_FORCE_SUGGESTS_=FALSE" > ~/.R/check.Renviron
 
-sudo tlmgr init-usertree
-sudo tlmgr update --self 
-sudo tlmgr install inconsolata upquote courier courier-scaled helvetic \
-                   verbatimbox readarray ifnextok multirow fancyvrb url \
-                   titlesec booktabs tex4ht
 # font 
 sudo tlmgr install ec times
 
@@ -62,8 +63,8 @@ fi
 
 R CMD INSTALL ${stanheadtargz}
 
-R -q -e "options(repos=structure(c(CRAN = 'http://cran.rstudio.com'))); for (pkg in c('inline', 'Rcpp', 'RcppEigen', 'RUnit', 'BH', 'RInside')) if (!require(pkg, character.only = TRUE))  install.packages(pkg, dep = TRUE); sessionInfo()"
 
+R -q -e "options(repos=structure(c(CRAN = 'http://cran.rstudio.com'))); for (pkg in c('inline', 'Rcpp', 'RcppEigen', 'RUnit', 'BH', 'RInside')) if (!require(pkg, character.only = TRUE))  install.packages(pkg, dep = TRUE); sessionInfo()"
 
 cd rstan
 echo "CXX = `R CMD config CXX`" >> R_Makevars # ccache is set in ~/.R/Makevars
