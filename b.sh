68741383626cf25712bcac77fa7ab9565182f2ec
maverickg <maverickg@users.noreply.github.com>
2016-06-11 21:40:06 -0400

    add installing pandoc

--- b.sh
+++ b.sh
@@ -51,7 +51,7 @@ sudo add-apt-repository -y "ppa:marutter/rrutter"
 sudo add-apt-repository -y "ppa:marutter/c2d4u"
 
 sudo apt-get -qq update
-sudo apt-get -qq -y install r-base-core qpdf texlive-latex-base texlive-base  xzdec texinfo ccache tex4ht texlive-fonts-extra r-cran-rcurl
+sudo apt-get -qq -y install pandoc r-base-core qpdf texlive-latex-base texlive-base  xzdec texinfo ccache tex4ht texlive-fonts-extra r-cran-rcurl
 sudo tlmgr init-usertree
 sudo tlmgr update --self 
 sudo tlmgr install upquote courier courier-scaled helvetic \
